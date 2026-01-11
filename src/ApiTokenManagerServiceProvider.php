2e3682c2ef2f25f481cfcd35b7a81f0f350ea966
Modelesque <ryan@modelesque.dev>
2026-01-11 19:55:10 +0100

    feat: updating service provider's register() and boot()

--- src/ApiTokenManagerServiceProvider.php
+++ src/ApiTokenManagerServiceProvider.php
@@ -2,17 +2,29 @@
 
 namespace Modelesque\ApiTokenManager;
 
+use Modelesque\ApiTokenManager\Contracts\ApiTokenRepositoryInterface;
+use Modelesque\ApiTokenManager\Contracts\PKCEAuthCodeFlowInterface;
+use Modelesque\ApiTokenManager\Repositories\EloquentApiTokenRepository;
+use Modelesque\ApiTokenManager\Services\Providers\PKCEAuthTokenProvider;
 use Illuminate\Support\ServiceProvider;
 
 class ApiTokenManagerServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-
+        $this->app->singleton(TokenManager::class);
+        $this->app->bind(PKCEAuthCodeFlowInterface::class, PKCEAuthTokenProvider::class);
+        $this->app->bind(ApiTokenRepositoryInterface::class, EloquentApiTokenRepository::class);
     }
 
     public function boot(): void
     {
-
+        // let hosts run this migration via:
+        // php artisan vendor:publish --tag=migrations
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/' => database_path('migrations'),
+            ], 'migrations');
+        }
     }
 }
