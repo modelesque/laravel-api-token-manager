568f37db455c4bd0fab2b100bf3809e24e9d0555
Modelesque <ryan@modelesque.dev>
2026-01-11 19:13:11 +0100

    chore: initial files

---
+++ src/ApiTokenManagerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modelesque\ApiTokenManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiTokenManagerServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
